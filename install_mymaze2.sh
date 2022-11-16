a6114203e69b93caef5f4e809fc79bc76702a205
slowrunner <slowrunner@users.noreply.github.com>
2022-11-16 17:48:12 -0500

    less obstacles, start not embedded

---
+++ install_mymaze2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Copying mymaze2.sdf to /opt/ros/galactic/share/turtlebot4_ignition_bringup/worlds/"
+sudo cp mymaze2.sdf /opt/ros/galactic/share/turtlebot4_ignition_bringup/worlds/
+echo "done"
--- sim_tb4lite.sh
+++ sim_tb4lite.sh
@@ -4,6 +4,6 @@ echo      "***************************************************************"
 echo -e "\n*** Starting TurtleBot 4 Lite Simulation in Ignition Gazebo ***"
 # echo "ros2 launch turtlebot4_ignition_bringup ignition.launch.py model:=lite"
 # ros2 launch turtlebot4_ignition_bringup ignition.launch.py model:=lite &
-echo "ros2 launch turtlebot4_ignition_bringup ignition.launch.py model:=lite world:=mymaze"
-ros2 launch turtlebot4_ignition_bringup ignition.launch.py model:=lite world:=mymaze
+echo "ros2 launch turtlebot4_ignition_bringup ignition.launch.py model:=lite world:=mymaze2"
+ros2 launch turtlebot4_ignition_bringup ignition.launch.py model:=lite world:=mymaze2
 
