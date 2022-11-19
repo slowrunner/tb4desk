3cdb0a05b7962df73053e3e22b524a58b3bc491f
slowrunner <slowrunner@users.noreply.github.com>
2022-11-19 10:53:11 -0500

    initials

---
+++ config/get_dds_file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "\n*** downloading galactic cyclonedds setup file"
+
+wget https://raw.githubusercontent.com/turtlebot/turtlebot4_setup/galactic/conf/cyclonedds_pc.xml
+
+echo "see https://turtlebot.github.io/turtlebot4-user-manual/overview/quick_start.html#network-configuration"
+
+
---
+++ reset_pose.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "\n*** RESET POSE ***"
+echo "ros2 service call reset_pose irobot_create_msgs/srv/ResetPose"
+ros2 service call reset_pose irobot_create_msgs/srv/ResetPose
